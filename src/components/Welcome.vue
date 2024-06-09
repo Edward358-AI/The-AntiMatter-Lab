c97dae3133249432406b5f21ee151767ee365657
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2024-06-09 02:08:40 +0000

    update timing for animations

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -7,7 +7,7 @@ defineProps(['sidebar'])
     <div id="welcome" :style="sidebar ? '' : 'transform: translateX(-160px);'">
         <img src="/logo.png" width="40%" class="rounded" />
         <h1>Welcome to the AntiMatter Lab.</h1>
-        <p style="animation: slideUp 5s forwards; animation-delay: 200ms; opacity: 0;">What is physics? It's the study
+        <p style="animation: slideUp 5s forwards; animation-delay: 100ms; opacity: 0;">What is physics? It's the study
             of matter.<br>Why learn physics? Because it matters.</p>
         <button type="button" class="btn btn-primary" @click="$emit('show-sidebar')"
             :style="sidebar ? 'visibility:hidden;' : ''">Get Started</button>
@@ -37,7 +37,7 @@ defineProps(['sidebar'])
 
 #welcome button {
     animation: slideUp 5s forwards;
-    animation-delay: 400ms;
+    animation-delay: 300ms;
     opacity: 0;
 }
 </style>
