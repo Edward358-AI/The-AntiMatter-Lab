1b414c3d6ab3a4162c3b9f318bae0024103f9e45
Hanyu Niu <antimatter5175@gmail.com>
2025-06-21 17:21:09 +0000

    finished ficticious forces

--- src/components/circularGravity/Fictious.vue
+++ src/components/circularGravity/Fictious.vue
@@ -141,23 +141,178 @@ defineEmits(["nextlesson", "nextpage", "prevpage"])
                 <br><br>
                 Hopefully you understood that. Please review this lesson if you didn't. I am no longer asking.
                 <br><br>
-                Now, back to our regularly scheduled program. We have a car cresting a circular hill, which can be approximated as
-                a portion of a circle with radius $R$. What is the maximum velocity it can have at any point, and what happens if it
-                exceeds that velocity? Assume it transits the hill with constant velocity for simplicity.
-                <br><br>
                 <figure>
                     <img src="/src/assets/dynamics/Figure 38.png" width = "500px">
                     <figcaption> Figure 4: A truck driving over a hill. No comments about the paint job allowed.</figcaption>
                 </figure>
                 <div v-show="level>0">
-                    Now, the first part of this question is simple to answer.
+                    Now, back to our regularly scheduled program. We have a car cresting a circular hill, which can be approximated as
+                    a portion of a circle with radius $R$. What is the maximum velocity it can have at the top, and what happens if it
+                    exceeds that velocity?
+                    <br><br>
+                    Now, the first part of this question is a bit simpler to answer. You have a single possible velocity for
+                    a given acceleration. Now, it might be tricky to figure out what the centripetal force is at the top, but
+                    remember that hills are vertical. The normal force varies since the net force has to equal the required centripetal
+                    force for circular motion at a certain velocity, and it opposes gravity. At the critical point, there is no more normal force and the entirety of the
+                    gravitational force contributes to the centripetal force. Therefore, at the top, the maximum possible centripetal
+                    acceleration is $a_c = g$. Therefore, we can write:
+                    <br><br>
+                    $g = \dfrac{v^2}{R}$
+                    <br><br>
+                    This simplifies to:
+                    <br><br>
+                    $v = \sqrt{gR}$
+                    <br><br>
+                    Technically, this is a maximum value since the normal force can vary and allow for other velocities. If you
+                    prefer, it is more accurate to write:
+                    <br><br>
+                    $v \leq \sqrt{gR}$
+                    <br><br>
+                    Now, this part did not require any invocation of ficticious forces, but the second part of the problem is easier
+                    if you invoke them. Think about how the ficticious centrifugal force would be directed. In the critical case, it is
+                    directed upwards and exactly equal in magnitude to the gravitational force. Exceeding this "speed limit" would make it
+                    such that the centrifugal force is greater than the gravitational, and the car would lift off the ground. This makes sense, 
+                    as if you travel over a bump in the road too fast you feel the car become airborne momentarily.
+                    <br><br>
                 </div>
+                <div v-show="level==0">
+                    Now, back to our regularly scheduled program. We have a car cresting a circular hill, which can be approximated as
+                    a portion of a circle with radius $R$. What forces are causing circular motion, and what happens if it goes too fast? Consider
+                    only the point where the car is at the top.
+                    <br><br>
+                    At the top of the hill, the radius of the hill is vertical, so we are only concerned with forces in the vertical direction.
+                    The only forces that can be in this direction are the gravitational and normal forces, and they are oppositely directed. In
+                    this scenario, $F_n < mg$ because the net force has to be downward to provide sufficient centripetal force for circular
+                    motion to occur. This is yet another case where the normal force is not equal to the weight of the object! For speeds low
+                    enough, the car will remain in contact with the ground, but there is a critical point where the normal force is zero and the
+                    entirety of the gravitational force is required to balance out the required centripetal force (or centrifugal force). This
+                    gives us the equation (it's one of the only ones you'll see here, I promise!):
+                    <br><br>
+                    $m\dfrac{v^2}{R} = mg$
+                    <br><br>
+                    Now, you can simplify this rather easily, but if you don't feel comfortable doing it, don't worry!
+                    You just need to know that the critical velocity where the normal force is zero is:
+                    <br><br>
+                    $v = \sqrt{gR}$
+                    <br><br>
+                    This is the maximum velocity at which circular motion is still possible. What happens if you go faster than this? Well,
+                    think of going over a speed bump really fast. Your car will go airborne momentarily. This happens because the ficticious
+                    centrifugal force upward is larger than gravity, which causes the car to lift off the ground from your perspective. In reality,
+                    you're really just experiencing inertia as your car fails to accelerate enough to go on its previous path and goes into a free-fall state, albeit only for a few fractions of a second.
+                    <br><br>
+                </div>
+                 Next, we will go over a few difficult force-balance problems that are easier with the addition of ficticious forces. They
+                    will not be solely focused on centripetal forces, but I still believe they are good to know.
             <div class="btn-contain-left">
                 <button class="btn btn-dark" style="animation: scale1 2s infinite;" @click="$emit('prevpage')">&larr;
                     Previous</button>
             </div>
             <div class="btn-contain-right">
                 <button class="btn btn-dark" style="animation: scale1 2s infinite;" @click="$emit('nextpage')">Next
+                    &rarr;</button>
+            </div>
+            </div>
+            <div v-show="page===2">
+            <div v-show="level>0">
+                Consider the scenario depicted in the figure below. What must the force $F$ be to hold all the blocks stationary
+                relative to each other? Assume there is no friction between all surfaces. The red circle is a pulley, in case
+                it wasn't clear. (It probably wasn't, considering how it looks.)
+                <br><br>
+                <figure>
+                    <img src="/src/assets/dynamics/Figure 39.png" width = "500px">
+                    <figcaption> Figure 5: The cart. I tried out a new aesthetic style.</figcaption>
+                </figure>
+                <br><br>
+                This problem is not trivial. It takes significant thought to figure out why this would even be possible
+                in the first place, since there is no friction. Most people see $m_3$ and conclude that it is impossible
+                for it to stay in place without any frictional forces. However, with ficticious forces things begin to
+                make sense.
+                <br><br>
+                The best approach to the problem is actually to start with the problematic block, $m_3$. It can easily accelerate
+                horizontally from a normal force applied on it by the big cart $m_1$, but it msut remain still vertically. We can actually
+                ignore this horizontal acceleration because the system moves as a whole, and it doesn't offer us any particularly useful
+                information. Since the block must remain at rest vertically, the tension force $T$ must obey the relation:
+                <br><br>
+                $T = m_3g$
+                <br><br>
+                Now, you might be really confused. For a normal Atwood's machine in this setup, the tension force is not this! However, this 
+                is the only way the third block can stay at rest. This leads us to the conclusion that this tension force also acts on
+                the second block. Now is where we invoke the ficticious force. Since the cart accelerates to the right due to a force
+                being exerted on it, we can assume it must have some acceleration $a$. This leads to a ficticious force to the left of 
+                magnitude $m_2a$ "applied" onto the second block, which must be balanced by the tension force. Thus, we have:
+                <br><br>
+                $T = m_2a$
+                <br><br>
+                This allows us to write:
+                <br><br>
+                $m_2a = m_3g$
+                <br><br>
+                $a = \dfrac{m_3}{m_2}g$
+                <br><br>
+                Now, we consider the force $F$. Since we have said that the system moves as a whole without any relative motion,
+                we can treat it as one body and only consider the external forces on it (which is just $F$ in this scenario):
+                <br><br>
+                $F = (m_1+m_2+m_3)a$
+                <br><br>
+                Substituting in our value for $a$ gives us:
+                <br><br>
+                $F = (m_1+m_2+m_3)\dfrac{m_3}{m_2}g$
+                <br><br>
+                That was difficult, right? But it's also an interesting problem that raises some interesting implications. Accelerated
+                reference frames can really mess with your usual intuitions, and postulates you held to be always true might not be
+                true when working with one! 
+                <br><br>
+                There is another interesting question I'd like to pose about this scenario. If all surfaces are frictionless, then how
+                do the blocks affect the motion of the cart?
+                <br><br>
+                The pulley is the answer to our question. We know (or assume logically) that the pulley is attached to the cart. The pulley
+                experiences tensional forces from either side as a consequence of Newton's third law, so the cart experiences those forces as well.
+                This is how the blocks can affect the cart despite being unable to interact with it through friction.
+                <br><br>
+            </div>
+            <div v-show="level==0">
+                 We are going to consider a relatively complex arrangement of blocks, pulleys, and a cart. Everything is frictionless
+                in this scenario. The red circle is a pulley.
+                <br><br>
+                <figure>
+                    <img src="/src/assets/dynamics/Figure 39.png" width = "500px">
+                    <figcaption> Figure 5: The cart. I tried out a new aesthetic style.</figcaption>
+                </figure>
+                <br><br>
+                We want to see if it's possible to push the cart such that none of the block slip or move relative to each
+                other. In other words, we want the cart and blocks to remain in place as we push it. Now, normally this
+                would seem impossible without friction, since $m_2$ cannot stop $m_3$ from sliding down because it is placed
+                horizontally. However, the blocks are accelerating.
+                <br><br>
+                Since there is a net external force represented by $F$ on the system, it will tend to accelerate to the right. This
+                means there will be a ficticious force to the left on each block. The only way block 3 can remain at rest is if the
+                tension force is equal to its weight, so we can assume that. Next, we know that this tension force is the only real
+                force acting in the horizontal direction on the second block. It is directed to the right, so we know that it has to
+                balance the ficticious force on the second block. This enables us to do force balance and makes the scenario physically possible.
+                <br><br>
+                The result we get from calculations is that there is only one possible acceleration where the system can remain at rest
+                relative to itself, which means by Newton's second law there is only one possible force $F$ that will enable this kind
+                of motion. An interesting result, to be sure.
+                <br><br>
+                We know that all three blocks accelerate together, so we can treat them as one object. But for them to move together, they
+                have to interact with each other through some force. However, it is not obvious how block 2 would interact with the cart. This
+                is a tricky question and requires very careful force analysis.
+                <br><br>
+                See, block 2 feels a tension force from the rope, which is constant throughout the rope. The pulley, which is attached to the 
+                cart, also feels this same tension force. This allows block 2 to "interact" with the cart and affect its motion, which enables us
+                to explain how block 2 would even affect the cart.
+                <br><br>
+            </div>
+                I hope I've shown you how working in an accelerated reference frame can be useful. Next, we're going to
+                pivot and begin to talk about gravity between celestial objects like the planets. This is quite different
+                from the gravitational forces we've dealt with previously.
+                <br><br>
+                <div class="btn-contain-left">
+                <button class="btn btn-dark" style="animation: scale1 2s infinite;" @click="$emit('prevpage')">&larr;
+                    Previous</button>
+            </div>
+            <div class="btn-contain-right">
+                <button class="btn btn-dark" style="animation: scale 2s infinite;" @click="$emit('nextlesson')">Next Lesson!
                     &rarr;</button>
             </div>
             </div>
