2a92e8615461b82babc8bcdac22773e9f3204606
Hanyu Niu <antimatter5175@gmail.com>
2025-07-03 23:48:14 +0000

    began power lesson

--- src/components/energy/Power.vue
+++ src/components/energy/Power.vue
@@ -8,8 +8,20 @@ defineEmits(["nextlesson", "nextpage", "prevpage"])
     <div class="container h100 p-5">
         <h1>Definition of Power</h1><br>
         <p>
-            
-            
+            <div v-show="page===0">
+                We say power is one of the three things people generally want, along with money and happiness. However, 
+                the way we think about power is more analgous to thinking about force or respect, rather than actual power. 
+                In fact, horses and oxen have much more "power" than humans. There's a reason why car engines are 
+                often measured in <i>horse</i>power and not <i>man</i>power!
+                <br><br>
+                Power is actually the rate at which something does work, or the rate at which it can change the energy of 
+                a system. Both definitions are pretty much analogous and can be referred to interchangeably. 
+
+                <div class="btn-contain-right">
+                <button class="btn btn-dark" style="animation: scale1 2s infinite;" @click="$emit('nextpage')">Next
+                    &rarr;</button>
+            </div>
+            </div>
         </p>
     </div>
 
