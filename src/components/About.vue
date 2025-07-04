245c8ffd3496e87a4f47d1d96e57ef03a6972be2
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2025-07-04 05:49:15 +0000

    edit the animations to be more crisp and snappy and add matter.js outline for running a demo

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,7 +11,7 @@
             where current resources were lacking. The website design and programming was done by Edward Jiang, and the
             curriculum/teachings were carefully crafted by multiple-time USAPHO qualifier Eric Niu. Our aim continues to
             be to make physics accessible, because we believe gaining a better understanding physics allows you to gain
-            a better understanding of yourself and the world around you.<br>This project is currently tending to lack in practice problems, you can find practice problems in this textbook, which also provides pretty good examples and demonstrations as well: <a href="https://drive.google.com/file/d/1hftVOFnduES1T_PrRzfTNhLNYu3OUSPc/view?usp=sharing" target="_blank">link here</a></p><br><br>
+            a better understanding of yourself and the world around you.<br>This project is currently tending to lack in practice problems, you can find practice problems in this textbook, which also provides pretty good examples and demonstrations as well: <a class="link-underline-secondary" href="https://drive.google.com/file/d/1hftVOFnduES1T_PrRzfTNhLNYu3OUSPc/view?usp=sharing" target="_blank">link here</a></p><br><br>
 
         <h1>Need Help?</h1><br>
         <p>If you just opened up this website or you're simply stuck on where to start, we recommend starting from the
@@ -19,7 +19,7 @@
             bar to find the exact lesson you want. The key difference between this and other physics tutorials out there
             is that this is built to cover three different levels of physics, which makes it suitable for all ages and
             all kinds of people.<br><br>
-            <span><strong>Conceptual:</strong> Simple as possible, avoiding using complex mathematical calculations/representations when possible. Prior experience using basic algebra is recommended, but not required. If you're brave, you could jump right in! However, it could a little more difficult to understand, however, if you don't have that background. If you don't, don't worry! Simple go to this link: <a href="https://www.khanacademy.org/math/algebra-basics/alg-basics-algebraic-expressions" target="_blank">https://www.khanacademy.org/math/algebra-basics/alg-basics-algebraic-expressions</a> and finish the whole thing, and then come back and start learning!</span><br>
+            <span><strong>Conceptual:</strong> Simple as possible, avoiding using complex mathematical calculations/representations when possible. Prior experience using basic algebra is recommended, but not required. If you're brave, you could jump right in! However, it could a little more difficult to understand, however, if you don't have that background. If you don't, don't worry! Simple go to this link: <a class="link-underline-secondary" href="https://www.khanacademy.org/math/algebra-basics/alg-basics-algebraic-expressions" target="_blank">https://www.khanacademy.org/math/algebra-basics/alg-basics-algebraic-expressions</a> and finish the whole thing, and then come back and start learning!</span><br>
             <br><br>
             <span><strong>Algebra-Based:</strong> Takes it up a notch by using algebra-level skills, which introduces a lot of new things like equations and graphs. Knowledge of Algebra 1 and Geometry is required, although we very strongly recommend studying Algebra 2 (either previously taken or concurrently learning) as it will make learning orders of magnitude easier.</span><br>
             <br><br>
@@ -30,18 +30,18 @@
         This is because some content may be overlapping (i.e. the content is the same regardless of what math level).</p><br><br>
 
         <h1>Technicalities</h1><br>
-        <p>This project was built using HTML, CSS and JS.<br>The use of VueJS (<a href="https://vuejs.org"
+        <p>This project was built using HTML, CSS and JS.<br>The use of VueJS (<a class="link-underline-secondary" href="https://vuejs.org"
                 target="_blank">https://vuejs.org</a>) was incorporated to allow dynamic rendering and much easier
-            implementation of certain functionalities over regular JS.<br>A CSS framework, Halfmoon CSS (<a
+            implementation of certain functionalities over regular JS.<br>A CSS framework, Halfmoon CSS (<a class="link-underline-secondary"
                 href="https://www.gethalfmoon.com" target="_blank">https://www.gethalfmoon.com</a>), was also used to
-            effortlessly create a (somewhat) nice looking layout and website.<br>MathJax (<a href="https://mathjax.org" target="_blank">https://mathjax.org</a>) and Vue-MathJax-Next (<a href="https://github.com/justforuse/vue-mathjax-next" target="_blank">https://github.com/justforuse/vue-mathjax-next</a>) were implemented for clean looking equations and mathematical symbols.<br>Vite (<a href="https://vitejs.dev"
+            effortlessly create a (somewhat) nice looking layout and website.<br>MathJax (<a class="link-underline-secondary" href="https://mathjax.org" target="_blank">https://mathjax.org</a>) and Vue-MathJax-Next (<a class="link-underline-secondary" href="https://github.com/justforuse/vue-mathjax-next" target="_blank">https://github.com/justforuse/vue-mathjax-next</a>) were implemented for clean looking equations and math symbols.<br>Vite (<a class="link-underline-secondary" href="https://vitejs.dev"
                 target="_blank">https://vitejs.dev</a>) was used during the development phase for convenience and is
             included in the production build as well.</p><br><br>
 
         <h1>Contact</h1><br>
         <p>If you have any questions, about physics or not, you can contact Edward via discord (edward.jiang_) or email
-            (<a href="mailto:edward456jiang@gmail.com" target="_blank">edward456jiang@gmail.com</a>). If the question is
-            physics related, you are most likely much better off asking Eric (discord: antimatter162, email: <a
+            (<a class="link-underline-secondary" href="mailto:edward456jiang@gmail.com" target="_blank">edward456jiang@gmail.com</a>). If the question is
+            physics related, you are most likely much better off asking Eric (discord: antimatter162, email: <a class="link-underline-secondary"
                 href="mailto:antimatter5175@gmail.com" target="_blank">antimatter5175@gmail.com</a>).</p><br><br>
 
         <h1>License</h1><br>
--- src/components/momentum/LinMomConsrv.vue
+++ src/components/momentum/LinMomConsrv.vue
@@ -1,21 +1,110 @@
 <script setup>
+// import { onMounted } from 'vue'
 defineProps(["level", "page"])
 defineEmits(["nextlesson", "nextpage", "prevpage"])
+
+
+// function runNewtonsCradle() {
+//     document.getElementById("newtoncradle").innerHTML = ""
+//     var Engine = Matter.Engine,
+//         Render = Matter.Render,
+//         Runner = Matter.Runner,
+//         Body = Matter.Body,
+//         Composites = Matter.Composites,
+//         MouseConstraint = Matter.MouseConstraint,
+//         Mouse = Matter.Mouse,
+//         Composite = Matter.Composite
+
+//     // create engine
+//     var engine = Engine.create(),
+//         world = engine.world
+
+//     // create renderer
+//     var render = Render.create({
+//         element: document.getElementById("newtoncradle"),
+//         engine: engine,
+//         options: {
+//             width: 600,
+//             height: 400,
+//             showVelocity: true,
+//             wireframes: false,
+//             background: "#000"
+//         }
+//     })
+
+//     Render.run(render)
+
+//     // create runner
+//     var runner = Runner.create()
+//     Runner.run(runner, engine)
+
+//     // see newtonsCradle function defined later in this file
+//     var cradle = newtonsCradle(180, 100, 5, 30, 200)
+//     Composite.add(world, cradle)
+//     Body.translate(cradle.bodies[0], { x: -150, y: -100 })
+
+//     // add mouse control
+//     var mouse = Mouse.create(render.canvas),
+//         mouseConstraint = MouseConstraint.create(engine, {
+//             mouse: mouse,
+//             constraint: {
+//                 stiffness: 0.2,
+//                 render: {
+//                     visible: false
+//                 }
+//             }
+//         })
+
+//     Composite.add(world, mouseConstraint)
+
+//     // keep the mouse in sync with rendering
+//     render.mouse = mouse
+
+//     // fit the render viewport to the scene
+//     Render.lookAt(render, {
+//         min: { x: 0, y: 50 },
+//         max: { x: 600, y: 400 }
+//     })
+// }
+
+// onMounted(() => {
+//     runNewtonsCradle()
+// })
+
+// function newtonsCradle(xx, yy, number, size, length) {
+//     var Composite = Matter.Composite,
+//         Constraint = Matter.Constraint,
+//         Bodies = Matter.Bodies
+
+//     var newtonsCradle = Composite.create({ label: 'Newtons Cradle' })
+
+//     for (var i = 0; i < number; i++) {
+//         var separation = 1.9,
+//             circle = Bodies.circle(xx + i * (size * separation), yy + length, size,
+//                 { inertia: Infinity, restitution: 1, friction: 0, frictionAir: 0, slop: size * 0.02 }),
+//             constraint = Constraint.create({ pointA: { x: xx + i * (size * separation), y: yy }, bodyB: circle })
+//             circle.render.fillStyle = "#001166"
+
+//         Composite.addBody(newtonsCradle, circle)
+//         Composite.addConstraint(newtonsCradle, constraint)
+//     }
+
+//     return newtonsCradle
+// }
 </script>
 
 
 <template>
     <div class="container h100 p-5">
         <h1>Conservation of Linear Momentum</h1><br>
+        <!-- <div id="newtoncradle"></div><br>
+        <button class="btn btn-outline-primary" @click="runNewtonsCradle()">Reset</button> -->
         <p>
-            
-            
+
         </p>
     </div>
 
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
