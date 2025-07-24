6a0bfbce0af059cb74ed85072acfa3753fa5265f
Edward358-AI <edward456jiang@gmail.com>
2025-07-24 09:36:59 -0700

    add warning message for small screens

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -18,6 +18,7 @@ const Window = window
         <p :style="sidebar ? 'animation: slideUp 5s forwards 400ms;opacity:0;' : 'opacity:0;'">Don't know where to
             go? Visit our about page!</p>
     </div>
+    <span class="warn mb-3 mx-3">{{ Window.innerWidth < 1000 ? "Note: This website is NOT optimized for smaller screens/viewport widths. While the website does work, some parts are best viewed on a larger screen." : "" }}</span>
 </template>
 
 <style scoped>
