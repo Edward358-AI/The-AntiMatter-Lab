778788c9f9b055598fc7546d07930818d36117cf
Edward358-AI <edward456jiang@gmail.com>
2025-07-30 21:51:11 -0700

    update the about page with more up to date information

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,7 +11,7 @@
             where current resources were lacking. The website design and programming was done by Edward Jiang, and the
             curriculum/teachings were carefully crafted by multiple-time USAPHO qualifier Eric Niu. Our aim continues to
             be to make physics accessible, because we believe gaining a better understanding physics allows you to gain
-            a better understanding of yourself and the world around you.<br>This project is currently tending to lack in
+            a better understanding of yourself and the world around you.<br>As of currently the project has reached its original goal of completing most if not all the major topics covered in mechanics, and regular content updates/new functionalities will likely be pushed out in the near future.<br><br>This project is currently tending to lack in
             practice problems, you can find practice problems in this textbook, which also provides pretty good examples
             and demonstrations as well: <a class="link-underline-secondary"
                 href="https://drive.google.com/file/d/1hftVOFnduES1T_PrRzfTNhLNYu3OUSPc/view?usp=sharing"
@@ -37,7 +37,7 @@
                 we very strongly recommend studying Algebra 2 (either previously taken or concurrently learning) as it
                 will make learning orders of magnitude easier.</span><br>
             <br><br>
-            <span><strong>Calculus-Based:</strong> The purest form of physics, which expands on algebra-based physics
+            <span><strong>Calculus-Based:</strong> The (nearly) purest form of physics, which expands on algebra-based physics
                 with calculus concepts. Concurrent learning or previously taken AP® Calculus AB (or of similar material)
                 is required for sufficient understanding.</span><br><br>
             <br>
@@ -59,7 +59,8 @@
                 class="link-underline-secondary" href="https://mathjax.org" target="_blank">https://mathjax.org</a>) and
             Vue-MathJax-Next (<a class="link-underline-secondary" href="https://github.com/justforuse/vue-mathjax-next"
                 target="_blank">https://github.com/justforuse/vue-mathjax-next</a>) were implemented for clean looking
-            equations and math symbols.<br>Vite (<a class="link-underline-secondary" href="https://vitejs.dev"
+            equations and math symbols.<br>Matter.js (<a class="link-underline-secondary" href="https://brm.io/matter-js/"
+                target="_blank">https://brm.io/matter-js</a>) is a rigid body 2d physics engine that runs on the web and is used to create the interactive demos found in some of our lessons.<br>Vite (<a class="link-underline-secondary" href="https://vitejs.dev"
                 target="_blank">https://vitejs.dev</a>) was used during the development phase for convenience and is
             included in the production build as well.</p><br><br>
 
