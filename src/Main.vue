24be92ed26b8632b2f12d386ab777a990caf0185
Edward358-AI <edward456jiang@gmail.com>
2025-08-02 23:18:08 -0700

    add basis for basic algebra lesson

--- src/Main.vue
+++ src/Main.vue
@@ -2,6 +2,7 @@
 import { ref, reactive, computed, watch, onMounted, onUpdated } from 'vue'
 import Welcome from './components/Welcome.vue'
 import About from './components/About.vue'
+import Algebra from './components/Algebra.vue'
 
 import Vectors from './components/kinematics/Vectors.vue'
 import PosVelAcc from './components/kinematics/PosVelAcc.vue'
@@ -57,7 +58,7 @@ const htmlElement = document.documentElement
 const doc = document
 const Window = window
 const user = reactive({
-  current: "landing", difficulty: 0, theme: "dark", page: {
+  current: "landing", difficulty: 0, theme: "dark", page: { Algebra: 0,
     Vectors: 0, DimenAnalyz: 0, PosVelAcc: 0, OneDMotion: 0, TwoDMotion: 0, RelativeVel: 0,
     FmaFBD: 0, Spring: 0, InclinedPlanes: 0, OtherForces: 0, Newton: 0,
     Kepler: 0, GravityLaws: 0, Centripetal: 0, Fictious: 0,
@@ -97,6 +98,7 @@ const sidebar = ref(false)
 const lessons = reactive(
   {
     kinematics: [
+      "Basic Algebra",
       "Vectors",
       "Dimensional Analysis",
       "Position, Velocity, Acceleration",
@@ -545,28 +547,32 @@ onUpdated(() => {
 
   <Welcome v-if="user.current === 'landing'" :sidebar="sidebar" @show-sidebar="sidebar = true; getCurrentPage()" @show-about="user.current='about'"/>
   <About v-if="user.current === 'about'" />
+  <Algebra v-if="user.current === lessons.kinematics[0]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    :page="user.page.Algebra" @nextpage="Window.scrollTo(0, 0); user.page.Algebra++"
+    @prevpage="Window.scrollTo(0, 0); user.page.Algebra--"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[1]; user.page.Algebra = 0" />
 
-  <Vectors v-if="user.current === lessons.kinematics[0]" :level="user.difficulty" :lessonShowing="lessonShowing"
+  <Vectors v-if="user.current === lessons.kinematics[1]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.Vectors" @nextpage="Window.scrollTo(0, 0); user.page.Vectors++"
     @prevpage="Window.scrollTo(0, 0); user.page.Vectors--"
-    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[1]; user.page.Vectors = 0" />
-  <DimenAnalyz v-if="user.current === lessons.kinematics[1]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[2]; user.page.Vectors = 0" />
+  <DimenAnalyz v-if="user.current === lessons.kinematics[2]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.DimenAnalyz" @nextpage="Window.scrollTo(0, 0); user.page.DimenAnalyz++"
     @prevpage="Window.scrollTo(0, 0); user.page.DimenAnalyz--"
-    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[2]; user.page.DimenAnalyz = 0" />
-  <PosVelAcc v-if="user.current === lessons.kinematics[2]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[3]; user.page.DimenAnalyz = 0" />
+  <PosVelAcc v-if="user.current === lessons.kinematics[3]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.PosVelAcc" @nextpage="Window.scrollTo(0, 0); user.page.PosVelAcc++"
     @prevpage="Window.scrollTo(0, 0); user.page.PosVelAcc--"
-    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[3]; user.page.PosVelAcc = 0" />
-  <OneDMotion v-if="user.current === lessons.kinematics[3]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[4]; user.page.PosVelAcc = 0" />
+  <OneDMotion v-if="user.current === lessons.kinematics[4]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.OneDMotion" @nextpage="Window.scrollTo(0, 0); user.page.OneDMotion++"
     @prevpage="Window.scrollTo(0, 0); user.page.OneDMotion--"
-    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[4]; user.page.OneDMotion = 0" />
-  <TwoDMotion v-if="user.current === lessons.kinematics[4]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[5]; user.page.OneDMotion = 0" />
+  <TwoDMotion v-if="user.current === lessons.kinematics[5]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.TwoDMotion" @nextpage="Window.scrollTo(0, 0); user.page.TwoDMotion++"
     @prevpage="Window.scrollTo(0, 0); user.page.TwoDMotion--"
-    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[5]; user.page.TwoDMotion = 0" />
-  <RelativeVel v-if="user.current === lessons.kinematics[5]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[6]; user.page.TwoDMotion = 0" />
+  <RelativeVel v-if="user.current === lessons.kinematics[6]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.RelativeVel" @nextpage="Window.scrollTo(0, 0); user.page.RelativeVel++"
     @prevpage="Window.scrollTo(0, 0); user.page.RelativeVel--"
     @nextlesson="Window.scrollTo(0, 0); user.current = lessons.dynamics[0]; user.page.RelativeVel = 0" />
---
+++ src/components/Algebra.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(["level", "page", "lessonShowing"])
+defineEmits(["nextlesson", "nextpage", "prevpage"])
+</script>
+
+<template>
+    <div class="container h100 p-5">
+        <h1>Basic Algebra</h1>
+            <div class="btn-contain-right">
+                <button class="btn btn-dark" style="animation: scale 2s infinite;" @click="$emit('nextlesson')">Next
+                    Lesson! &rarr;</button>
+            </div>
+    </div>
+</template>
+
+<style scoped></style>
