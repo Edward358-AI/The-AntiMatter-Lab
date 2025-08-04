e587ca87be6e63695e9a1fe1c642ffe964c1abe9
Edward358-AI <edward456jiang@gmail.com>
2025-08-03 17:31:18 -0700

    add trig base

--- src/Main.vue
+++ src/Main.vue
@@ -6,6 +6,7 @@ import About from './components/basics/About.vue'
 import Algebra from './components/basics/Algebra.vue'
 import GettingStarted from './components/basics/GettingStarted.vue'
 import MetricPrefix from './components/basics/MetricPrefix.vue'
+import Trig from './components/basics/Trig.vue'
 
 import Vectors from './components/kinematics/Vectors.vue'
 import PosVelAcc from './components/kinematics/PosVelAcc.vue'
@@ -61,7 +62,7 @@ const htmlElement = document.documentElement
 const doc = document
 const Window = window
 const user = reactive({
-  current: "landing", difficulty: 0, theme: "dark", page: { Algebra: 0, About: 0, GettingStarted: 0, metricPrefix: 0,
+  current: "landing", difficulty: 0, theme: "dark", page: { Algebra: 0, About: 0, GettingStarted: 0, metricPrefix: 0, Trig: 0,
     Vectors: 0, DimenAnalyz: 0, PosVelAcc: 0, OneDMotion: 0, TwoDMotion: 0, RelativeVel: 0,
     FmaFBD: 0, Spring: 0, InclinedPlanes: 0, OtherForces: 0, Newton: 0,
     Kepler: 0, GravityLaws: 0, Centripetal: 0, Fictious: 0,
@@ -104,7 +105,8 @@ const lessons = reactive(
       "About Us and Our Mission", 
       "Getting Started",
       "Basic Algebra",
-      "Metric Prefixes"
+      "Metric Prefixes",
+      "Trigonometry"
     ],
     kinematics: [
       "Vectors",
@@ -600,7 +602,11 @@ onUpdated(() => {
   <MetricPrefix v-if="user.current === lessons.basics[3]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.metricPrefix" @nextpage="Window.scrollTo(0, 0); user.page.metricPrefix++"
     @prevpage="Window.scrollTo(0, 0); user.page.metricPrefix--"
-    @nextlesson="Window.scrollTo(0, 0); user.current = lessons. kinematics[0]; user.page.metricPrefix = 0" />
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.basics[4]; user.page.metricPrefix = 0" />
+  <Trig v-if="user.current === lessons.basics[4]" :level="user.difficulty" :lessonShowing="lessonShowing"
+    :page="user.page.Trig" @nextpage="Window.scrollTo(0,0); user.page.Trig++"
+    @prevpage="Window.scrollTo(0, 0); user.page.Trig--"
+    @nextlesson="Window.scrollTo(0, 0); user.current = lessons.kinematics[0]; user.page.Trig = 0" />
 
   <Vectors v-if="user.current === lessons.kinematics[0]" :level="user.difficulty" :lessonShowing="lessonShowing"
     :page="user.page.Vectors" @nextpage="Window.scrollTo(0, 0); user.page.Vectors++"
---
+++ src/components/basics/Trig.vue
@@ -0,0 +1,306 @@
+<script setup>
+import { reactive, watch, ref } from 'vue'
+const props = defineProps(["level", "page", "lessonShowing"])
+defineEmits(["nextlesson", "nextpage", "prevpage"])
+
+const show = ref(false)
+const show1 = ref(false)
+const show2 = ref(false)
+const show3 = ref(false)
+const show4 = ref(false)
+
+const results = reactive([[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]]) // update as add more questions
+const explanations = reactive([[false,false,false,false,false,false], [false,false,false,false,false,false], [false,false,false,false,false,false]]) // keeps track of what explanations are visible
+const questions = reactive(
+    [
+        [ // conceptual difficulty
+            {
+                number: 0,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 1,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 2,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 3,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 4,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 5,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            }
+        ],
+        [ // algebra difficulty
+            {
+                number: 0,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 1,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 2,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 3,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 4,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 5,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            }
+        ],
+        [ // calculus difficulty
+            {
+                number: 0,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 1,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 2,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 3,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 4,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            },
+            {
+                number: 5,
+                question: "",
+                answers: [
+                    ["", 0, false],
+                    ["", 1, false],
+                    ["", 0, false],
+                    ["", 0, false]
+                ],
+                explain: ""
+            }
+        ]
+    ]
+)
+
+// universal check answer for a given question
+function checkAnswer(form) {
+    const data = new FormData(document.querySelectorAll(".question")[form])
+    if (data.get("question") === "y") results[props.level][form] = 1
+    else results[props.level][form] = -1
+}
+
+function setChecked(chek, qNum) {
+    for (let i = 0; i < questions[props.level][qNum].answers.length; i++) {
+        if (questions[props.level][qNum].answers[i][2] && i !== chek) {
+            questions[props.level][qNum].answers[i][2] = false
+        } else if (!questions[props.level][qNum].answers[i][2] && i === chek) {
+            questions[props.level][qNum].answers[i][2] = true
+        }
+    }
+}
+</script>
+
+<template>
+    <div v-show="lessonShowing" class="container h100 p-5">
+        <h1>Trigonometry</h1><br>
+        <div v-show="page === 0">
+            <p>
+                
+            </p>
+            <div class="btn-contain-right">
+                <button class="btn btn-dark" style="animation: scale1 2s infinite;" @click="$emit('nextpage')">Next
+                    &rarr;</button>
+            </div>
+        </div>
+        <div v-show="page === 1">
+            <p>
+               
+            </p>
+            <div class="btn-contain-left">
+                <button class="btn btn-dark" style="animation: scale1 2s infinite;" @click="$emit('prevpage')">&larr;
+                    Previous</button>
+            </div>
+            <div class="btn-contain-right">
+                <button class="btn btn-dark" style="animation: scale 2s infinite;" @click="$emit('nextlesson')">Next
+                    Lesson!
+                    &rarr;</button>
+            </div>
+        </div>
+    </div>
+
+    <div v-show="!lessonShowing" class="container h100 pt-5">
+        <h1>Metric Prefixes Problems</h1><br>
+        <div class="question-container row justify-content-center mx-auto pb-5">
+            <form @submit.prevent="checkAnswer(q.number)" style="height:fit-content"
+                class="question col-6 row justify-content-center my-5 mx-auto" v-for="q in questions[level]">
+                <div class="w-100">
+                    <label class="form-label fs-5" v-html=" q.number + 1 + '. ' + q.question"></label><br>
+                </div>
+                <div class="col border-end border-secondary">
+                    <div class="ms-auto" style="width:fit-content">
+                        <div class="form-check" style="width:fit-content;" v-for="(a, index) in q.answers">
+                            <input class="form-check-input" type="radio" name="question" :value="a[1] === 0 ? 'n' : 'y'"
+                                :checked="a[2]" @click="setChecked(index, q.number)">
+                            <label class="form-check-label text-start" style="font-size:0.96rem; max-width: 200px;">
+                                {{ a[0] }}
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex flex-column text-start">
+                    <input class="btn btn-primary d-block me-auto my-auto" type="submit"
+                        :value="results[level][q.number] !== 0 ? 'Check Again' : 'Check Answer'"><br>
+                    <div class="me-auto mb-auto" v-show="results[level][q.number] !== 0">{{ results[level][q.number]
+                        === 1 ?
+                        "&#x2705; Correct!" : "&#x274c; Not quite! Try again." }}
+                    </div>
+                    <a href="javascript:void(0)" v-show="results[level][q.number] !== 0" class="me-auto mb-auto ms-1"
+                        @click="explanations[level][q.number] = !explanations[level][q.number]">{{
+                            !explanations[level][q.number] ? "Want to see an explanation? " : "Hide explanation" }}</a>
+                </div>
+                <span class="explained mt-3" v-show="explanations[level][q.number]" v-html="q.explain"></span>
+            </form>
+
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
