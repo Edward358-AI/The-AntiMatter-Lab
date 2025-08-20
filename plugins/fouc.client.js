a52c174045160e868de61fd7beaa5bc3d00070de
Edward358-AI <edward456jiang@gmail.com>
2025-08-19 18:34:22 -0700

    undo previous commit

--- plugins/fouc.client.js
+++
@@ -1,7 +0,0 @@
-export default defineNuxtPlugin(() => {
-  window.onload = () => {
-    document.getElementById("__nuxt").style.opacity = 1
-    document.body.style.opacity = 1
-    document.documentElement.style.opacity = 1
-  }
-})
